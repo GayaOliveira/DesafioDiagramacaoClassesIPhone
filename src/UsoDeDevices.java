1fdd86ae9dbd896caa102d2e3836dd7237ace1cc
GayaOliveira <gmo01233210@gmail.com>
2024-05-12 22:04:03 -0300

    Criação da classe UsoDeDevices, em que instancio e utilizo um objeto iPhone

---
+++ src/UsoDeDevices.java
@@ -0,0 +1,17 @@
+public class UsoDeDevices {
+    public static void main(String[] args) {
+
+        IPhone iPhone = new IPhone();
+
+        iPhone.tocar();
+        iPhone.pausar();
+        iPhone.selecionarMusica();
+        iPhone.ligar();
+        iPhone.atender();
+        iPhone.iniciarCorreioVoz();
+        iPhone.exibirPagina();
+        iPhone.adicionarNovaAba();
+        iPhone.atualizarPagina();
+
+    }
+}
